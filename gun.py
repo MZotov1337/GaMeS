5ae1d9af004ad30c2fcfb6a45e7927f48b2ed69d
Mikhail Zotov <zotov.md@phystech.edu>
2019-10-28 22:00:28 +0300

    YA SDELYAL URAAAAA

--- gun.py
+++ gun.py
@@ -52,32 +52,28 @@ class ball():
         Рё СЃС‚РµРЅ РїРѕ РєСЂР°СЏРј РѕРєРЅР° (СЂР°Р·РјРµСЂ РѕРєРЅР° 800С…600).
         """
         if self.y <= 500:
-        self.vy -= 1.2
-        self.y -= self.vy
-        self.x += self.vx
-        self.vx *= 0.99
-        self.set_coords()
-    else:
-        if self.vx**2 + self.vy**2 > 10:
-            self.vy =- self.vy / 2
-            self.vx = self.vx / 2
-            self.y = 499
-        if self.live < 0:
-            balls.pop(balls.index(self))
-            canv.delete(self.id)
+            self.vy -= 1.2
+            self.y -= self.vy
+            self.x += self.vx
+            self.vx *= 0.99
+            self.set_coords()
         else:
-            self.live -= 1
-    if self.x > 780:
-        self.vx =- self.vx / 2
-        self.x = 779
-    def hittest(self,ob):
-        if abs(ob.x - self.x) <= (self.r + ob.r) and abs(ob.y - self.y) <= (self.r + ob.r):
-            return True
-        else:
-            return False
+            if self.vx**2 + self.vy**2 > 10:
+                self.vy =- self.vy / 2
+                self.vx = self.vx / 2
+                self.y = 499
+            if self.live < 0:
+                balls.pop(balls.index(self))
+                canv.delete(self.id)    
+            else:
+                self.live -= 1
+        if self.x > 780:
+            self.vx -= self.vx / 2
+            self.x = 779
+    
             
 
-    def hittest(self, obj):
+    def hittest(self, ob):
         """Р¤СѓРЅРєС†РёСЏ РїСЂРѕРІРµСЂСЏРµС‚ СЃС‚Р°Р»РєРёРІР°Р»РєРёРІР°РµС‚СЃСЏ Р»Рё РґР°РЅРЅС‹Р№ РѕР±СЊРµРєС‚ СЃ С†РµР»СЊСЋ, РѕРїРёСЃС‹РІР°РµРјРѕР№ РІ РѕР±СЊРµРєС‚Рµ obj.
 
         Args:
@@ -85,6 +81,10 @@ class ball():
         Returns:
             Р’РѕР·РІСЂР°С‰Р°РµС‚ True РІ СЃР»СѓС‡Р°Рµ СЃС‚РѕР»РєРЅРѕРІРµРЅРёСЏ РјСЏС‡Р° Рё С†РµР»Рё. Р’ РїСЂРѕС‚РёРІРЅРѕРј СЃР»СѓС‡Р°Рµ РІРѕР·РІСЂР°С‰Р°РµС‚ False.
         """
+        if abs(ob.x - self.x) <= (self.r + ob.r) and abs(ob.y - self.y) <= (self.r + ob.r):
+            return True
+        else:
+            return False        
         # FIXME
             #return False
 
@@ -107,12 +107,17 @@ class gun():
         """
         global balls, bullet
         bullet += 1
+        
         new_ball = ball()
         new_ball.r += 5
+        
         self.an = math.atan((event.y-new_ball.y) / (event.x-new_ball.x))
+        
         new_ball.vx = self.f2_power * math.cos(self.an)
         new_ball.vy = - self.f2_power * math.sin(self.an)
+        
         balls += [new_ball]
+        
         self.f2_on = 0
         self.f2_power = 10
 
@@ -140,12 +145,12 @@ class gun():
 
 class target():
     def __init__(self):
-     self.points = 0
-     self.live = 1
-     self.id = canv.create_oval(0, 0, 0, 0)
-     self.id_points = canv.create_text(30, 30, text=self.points, font='28')
-     self.new_target()
-   
+        self.points = 0
+     
+        self.id = canv.create_oval(0, 0, 0, 0)
+        self.id_points = canv.create_text(30, 30, text = self.points, font = '28')
+        self.new_target()
+        self.live = 1
     # FIXME: don't work!!! How to call this functions when object is created?
     # self.id = canv.create_oval(0,0,0,0)
     # self.id_points = canv.create_text(30,30,text = self.points,font = '28')
@@ -193,7 +198,7 @@ def new_game(event=''):
                 t1.hit()
                 canv.bind('<Button-1>', '')
                 canv.bind('<ButtonRelease-1>', '')
-                canv.itemconfig(screen1, text='Р’С‹ СѓРЅРёС‡С‚РѕР¶РёР»Рё С†РµР»СЊ Р·Р° ' + str(bullet) + ' РІС‹СЃС‚СЂРµР»РѕРІ')
+                canv.itemconfig(screen1, text='Вы уничтожили цель за ' + str(bullet) + ' выстрелов')
         canv.update()
         time.sleep(0.03)
         g1.targetting()
@@ -204,5 +209,4 @@ def new_game(event=''):
 
 
 new_game()
-
-mainloop()
+root.mainloop()
